0a9cc654e95f3f305172f02f3ba8a677d7d9522d
ARSHA PURUSHAN <147630900+ArshaPurushan@users.noreply.github.com>
2023-11-29 15:14:02 +0530

    Add files via upload

--- Graphics/DGraphics/Module2.py
+++ Graphics/DGraphics/Module2.py
@@ -1,5 +1,13 @@
+import math
 def cuboid(l,w,h):
     a=2*(l*w+l*h+h*w)
-    p=l*w*h
-    print("Area: ",a)
-    print("Perimeter: ",p)
+    v=l*w*h
+    print("Area of cuboid: ",a)
+    print("Volume of cuboid: ",v)
+
+def sphere(r):
+    a=4*math.pi*r*r
+    v=(4/3)*math.pi*r*r*r
+    print("Area of sphere: ",a)
+    print("Volume of sphere: ",v)
+    
